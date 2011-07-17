2fd26a9a68054f1a27804e6ede87759c16cab034
Jonathan Kim <jkimbo@gmail.com>
2011-07-17 18:03:24 +0100

    Got socket io working!!!!

--- server.js
+++ server.js
@@ -10,38 +10,9 @@ var io = io.listen(server),
   buffer = {},
   users = {};
  
-function oc(a) {
-  var o = {};
-  for(var i=0;i<a.length;i++) {
-    o[a[i]]='';
-  }
-  return o;
-}
-
 io.sockets.on('connection', function(socket) {
 	
-	socket.on('user message', function (msg) {
-		socket.broadcast.emit('user message', socket.nickname, msg);
-	});
-
-	socket.on('nickname', function (nick, fn) {
-		if (users[nick]) {
-			fn(true);
-		} else {
-			fn(false);
-			users[nick] = socket.user = nick;
-			socket.broadcast.emit('announcement', nick + ' connected');
-			io.sockets.emit('users', users);
-		}
-	});
-	
-	socket.on('disconnect', function () {
-		if (!socket.nickname) return;
-
-		delete nicknames[socket.nickname];
-		socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-		socket.broadcast.emit('nicknames', nicknames);
-	});
+	socket.emit('message', { hello: 'world' });
 
 });
 
